a2991ead8b60e3461851183f9f69ccb7317053cd
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2016-01-01 17:06:30 -0800

    Building a Static Library (.a)

---
+++ staticlib/include/Student.h
@@ -0,0 +1,11 @@
+#ifndef Student_H
+#define Student_H
+class Student{
+    private:
+    int age;
+    public:
+    Student();
+    Student(int age1);
+    int getAge();
+};
+#endif
---
+++ staticlib/src/Student.cpp
@@ -0,0 +1,10 @@
+#include "Student.h"
+
+Student::Student(){}
+
+Student::Student(int age1):age(age1){}
+
+int Student::getAge()
+{
+    return age;
+}
